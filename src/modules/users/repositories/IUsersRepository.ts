239c611039ca3d38e07e1219596852dad1593ca8
GrazielaFerreira <grazyeter@gmail.com>
2021-03-28 14:07:13 -0300

    Initial commit

---
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { User } from "../model/User";
+
+interface ICreateUserDTO {
+  name: string;
+  email: string;
+}
+
+interface IUsersRepository {
+  create({ name, email }: ICreateUserDTO): User;
+  findById(id: string): User | undefined;
+  findByEmail(email: string): User | undefined;
+  turnAdmin(user: User): User;
+  list(): User[];
+}
+
+export { IUsersRepository, ICreateUserDTO };
